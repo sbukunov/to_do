293facf5dfd45b75d090daaead3cca58075ccdc4
serj <sergeybukunov@yandex.ru>
2022-06-03 14:15:48 +0300

    refactor filters.py

--- note_api/filters.py
+++ note_api/filters.py
@@ -4,13 +4,13 @@ from django.db.models import QuerySet
 from note import models
 
 # Фильтр по важности
-def note_by_important_filter(queryset: QuerySet, important: Optional[bool]): # Фильтр по важности
+def note_by_important_filter(queryset: QuerySet, important: Optional[bool]):
     return queryset.filter(important=important)
 
 # Фильтр по публичности
-def note_by_public_filter(queryset: QuerySet, public: Optional[bool]): # Фильтр по важности
+def note_by_public_filter(queryset: QuerySet, public: Optional[bool]):
     return queryset.filter(public=public)
 
-# Фильтр по публичности
-def note_by_status_filter(queryset: QuerySet, status: Optional[int]): # Фильтр по важности
+# Фильтр по статусу
+def note_by_status_filter(queryset: QuerySet, status: Optional[int]):
     return queryset.filter(status=status)
